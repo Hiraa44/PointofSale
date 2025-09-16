214ffffa16cb4b0a6701726b72793ed1d93048ca
Hiraa44 <alhira23@gmail.com>
2025-09-16 07:50:55 +0500

    This is my First commit

---
+++ mysite/POS/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price= models.DecimalField(max_digits=4, decimal_places=3)
+    availability = models.BooleanField(default=True)
+    created_at = models.DateField(auto_now_add=True)
+
+class Login(models.Model):
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    username = models.CharField(max_length=300, default="guest")
+    
+    #def __str__(self):
+     #return(self.name)
+
+
+
---
+++ mysite/POS/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Login
+import requests
+# Create your views here.
+def Home(request):
+  if request.method == 'POST':
+    email = request.POST.get("email")
+    password = request.POST.get("password")
+    username = request.POST.get("username")
+    if email and password and username:
+      login=Login(email=email,password=password)
+      login.save()  
+      return render(request,'home.html')
+    else:
+    
+     error_message = "Please fill all the required fields"
+     return render(request, "index.html", {'error_message': error_message})
+
+  return render(request, "index.html")
+  
