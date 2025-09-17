4a0ef0bf73d10e10063e62a1fc3bf7ea706c37b8
Hiraa44 <alhira23@gmail.com>
2025-09-17 15:20:01 +0500

    F342

--- mysite/POS/views.py
+++ mysite/POS/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Login
+from .models import Signup
 import requests
 # Create your views here.
 def Home(request):
@@ -7,20 +7,26 @@ def Home(request):
     email = request.POST.get("email")
     password = request.POST.get("password")
     username = request.POST.get("username")
-    if Login.objects.filter(email=email).exists():
+    if Signup.objects.filter(email=email).exists():
       print("User already exists")
       return render(request, "signup.html", {"email": email})
     if email and password and username:
-      login=Login(email=email,password=password)
-      login.save()  
+      signup=Signup(email=email,password=password, username=username)
+      signup.save()  
       return render(request,'home.html')
     else:
-    
      error_message = "Please fill all the required fields"
      return render(request, "signup.html", {'error_message': error_message})
 
   return render(request, "signup.html")
 def login(request):
+  if request.method == 'POST':
+    username = request.POST.get("username")
+    if Signup.objects.filter(username=username).exists():
+     return render(request, "home.html",{"uname": username})
+    else:
+     error = "User does not exist with this"
+     return render(request,"login.html", {'login_error' : error, "uname": username} )
   return render(request,"login.html")
 def signup(request):
   return render(request,"signup.html")
